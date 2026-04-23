99e5613392ee509374b461c171313e3810f36d1e
mcartage32 <mcartage33@gmail.com>
2026-04-23 15:38:29 -0500

    Se implementa Bearer token en los endpoints de los leads

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { JwtModule } from '@nestjs/jwt';
       signOptions: { expiresIn: '6h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/leads/leads.controller.ts
+++ src/leads/leads.controller.ts
@@ -7,39 +7,47 @@ import {
   Patch,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { LeadsService } from './leads.service';
 import { CreateLeadDto, GetLeadsDto } from './leads.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('leads')
 export class LeadsController {
   constructor(private readonly service: LeadsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() dto: CreateLeadDto) {
     return this.service.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(@Query() query: GetLeadsDto) {
     return this.service.findAll(query);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('stats')
   stats() {
     return this.service.getStats();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.service.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: Partial<CreateLeadDto>) {
     return this.service.update(id, dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.service.remove(id);
